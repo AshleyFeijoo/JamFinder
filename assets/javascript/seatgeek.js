7d5b6902e2419ba62e503a3029db8ea7e6c81831
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-05-13 13:30:17 -0500

    changes

--- assets/javascript/seatgeek.js
+++ assets/javascript/seatgeek.js
@@ -1,6 +1,8 @@
 
       // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
       $(document).ready(function () { 
+        M.AutoInit();
+
       $('select').formSelect();
 
       $('.dropdown-trigger').dropdown();
