90b78b4201d5ad4cfef1341da250048055278667
Ellen Buelow <buelowek1225@gmail.com>
2019-05-13 15:08:10 -0500

    seatgeak updates table

--- assets/javascript/seatgeek.js
+++ assets/javascript/seatgeek.js
@@ -1,51 +1,82 @@
 
-      // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
-          
-      function getConcertByLatLon(lat, lon, range, ticketPrice, datetime){
+  // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+  var latStart;
+  var lonStart;
 
-        var client_id = "MTEyMTc0NzN8MTU1NzM0NDE0OS40OA";
-        var client_secret = "d6005bfa21771638a4b460529bda0a83178316ba8c20d7ed24f0a383973f6246";
-        // var listingCount = "10";
-        var baseURL = "https://api.seatgeek.com/2/"; 
-        var endpoint = "events";
+  navigator.geolocation.getCurrentPosition(function(location) {
+    latStart = (location.coords.latitude);
+    lonStart = (location.coords.longitude);
+    console.log(latStart);
+    console.log(lonStart);
+  });    
 
-        var lat = lat;
-        var latString = "&lat=" + lat
+  function getConcertByLatLon(lat, lon, range, ticketPrice, datetime){
 
-        var lon = lon;
-        var lonString = "&lon=" + lon
+    var client_id = "MTEyMTc0NzN8MTU1NzM0NDE0OS40OA";
+    var client_secret = "d6005bfa21771638a4b460529bda0a83178316ba8c20d7ed24f0a383973f6246";
+    // var listingCount = "10";
+    var baseURL = "https://api.seatgeek.com/2/"; 
+    var endpoint = "events";
 
-        var range = range;
-        var rangeString = "&range.lte=" + range
+    var lat = lat;
+    var latString = "&lat=" + lat;
 
-        var ticketPrice = ticketPrice;
-        var ticketPriceString = "&lowest_price.lte=" + ticketPrice; 
+    var lon = lon;
+    var lonString = "&lon=" + lon;
 
-        var datetime = datetime;
-        var datetimeString = "&datetime_local.lte=" + datetime;
+    var range = range;
+    var rangeString = "&range.lte=" + range;
 
-        var taxonomy = "concert";
-        var taxonomyString = "&taxonomies.name=" + taxonomy;
+    var ticketPrice = ticketPrice;
+    var ticketPriceString = "&lowest_price.lte=" + ticketPrice; 
 
-        var queryURL = baseURL + endpoint + "/?client_id=" + client_id + "&client_secret=" + client_secret + latString + lonString + rangeString + ticketPriceString + datetimeString + taxonomyString; 
+    var datetime = datetime;
+    var datetimeString = "&datetime_local.lte=" + datetime;
+
+    var taxonomy = "concert";
+    var taxonomyString = "&taxonomies.name=" + taxonomy;
+
+    var queryURL = baseURL + endpoint + "/?client_id=" + client_id + "&client_secret=" + client_secret + latString + lonString + rangeString + ticketPriceString + datetimeString + taxonomyString;
+
+    console.log(queryURL);
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // then call function to update table    
+      // function updateMusicTable(){
+        console.log(response);
+        for (i = 0; i < response.events.length; i++){
+          // console.log(response.events[0].title);
+          var eventTitle = response.events[i].title;
+          // console.log(response.events[0].venue.name);
+          var venueName = response.events[i].venue.name;
+          // console.log(response.events[0].datetime_local);
+          var eventLocalTime = response.events[i].datetime_local;
+          // console.log(response.events[0].stats.average_price);
+          var eventAveragePrice = response.events[i].stats.average_price;
+          var tableLineData = "<tr><td>" + venueName + "</td><td>" + eventLocalTime + "</td><td>" + eventTitle + "</td><td>" + eventAveragePrice + "</td><td>";
+          $("table tbody").append(tableLineData);
+        };
 
         
+      // }
 
-        console.log(queryURL);
+      // updateMusicTable(0);
 
-        // $.ajax({
-        //   url: queryURL,
-        //   method: "GET"
-        // }).then(function(response) {
-        //     console.log(response);
-        //     for (i=0; i < 21; i++)
-        //     console.log(response.genres[i].name);
-        // });
-
-      }
-
-    getConcertByLatLon("44.98", "-93.18", "200mi", "10", "2019-05-17");
+      // updateMusicTable(tableLineData);
+      // we have our response now we want to set variable to the div ids
+    });
     
-    
-      
+  };
+
+  // calling our function that spells out our queryURL
+  getConcertByLatLon("44.98", "-94.18", "200mi", "200", "2019-05-17");
+
+  // updateMusicTable(tableLineData);
+
      
+  
+
+  
